6e151411e7212c2a0379030931964927370c09cf
Jhonny-Tis <jhonny.h.cr@gmail.com>
2014-12-02 23:42:36 -0400

    Nuevo Subir Documento GE

---
+++ Vista/GuardarSubirDocumento.php
@@ -0,0 +1,58 @@
+<?php
+
+include '../Modelo/conexion.php';
+
+session_start();
+$UsuarioActivo = $_SESSION['usuario'];
+$rutaDirectorio="../Repositorio/$UsuarioActivo";  //ruta de nuestro directorio
+$asesor = $UsuarioActivo;   
+$clas = new conexion();
+
+    if(!file_exists($rutaDirectorio))
+    {
+        mkdir($rutaDirectorio, 0777);
+    }
+    
+    $ruta = "$rutaDirectorio/" . $_FILES['archivoA']['name'];
+            $rutaDocumento="/Repositorio/$UsuarioActivo/" . $_FILES['archivoA']['name'];
+            //ahora movemos el archivo
+            try{
+            $resultado = move_uploaded_file($_FILES['archivoA']['tmp_name'], $ruta);
+            if ($resultado) {
+                
+                //recuperamos la idRegistro siguiente que se insertara en la BD de registro para enviarlo a documento
+                $resultadoUno=$clas->consulta("SELECT auto_increment FROM `information_schema`.tables WHERE TABLE_SCHEMA = 'saetis' AND TABLE_NAME = 'registro'");
+                while ($filas = mysql_fetch_array($resultadoUno)) {
+                    $idRegistro=(integer)$filas['0'];
+                }
+                
+                $nombre = $_FILES['archivoA']['name'];
+                $tamanio =(integer) $_FILES['archivoA']['size'];
+                $visualizable=TRUE;
+                $descargable=TRUE;
+                date_default_timezone_set('America/La_Paz');
+                $fecha=  date('Y-m-d');
+                $hora=  date("G:H:i");
+                $clas->consulta("INSERT INTO `registro` (NOMBRE_U,TIPO_T,ESTADO_E,NOMBRE_R,FECHA_R,HORA_R)  VALUES ('$UsuarioActivo','documento subido','habilitado','$nombre','$fecha','$hora')");
+                $clas->consulta("INSERT INTO `documento` (ID_R,TAMANIO_D,RUTA_D,VISUALIZABLE_D,DESCARGABLE_D) VALUES ($idRegistro,$tamanio,'$rutaDocumento','$visualizable','$descargable')");
+                echo 'Documento cargado exitosamente';
+                
+            }
+            }
+             catch (Exception $e) {
+                    echo 'Ha ocurrido un error: ',  $e->getMessage(), "\n";
+                }
+            
+            
+        //}
+        //else
+        //{
+            //echo 'el formato de archivo o el tamanio de archivo no son permitidos';
+        //}
+
+$clas->cerrarConexion();
+    
+   
+
+
+?>
---
+++ Vista/SubirDocumento.php
@@ -0,0 +1,288 @@
+
+<!DOCTYPE html>
+<?php
+    include '../Modelo/conexion.php';
+    session_start();
+    $con=new conexion();
+    $UsuarioActivo = $_SESSION['usuario'];
+?>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sistema de Apoyo a la Empresa TIS</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="../Librerias/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../Librerias/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Page-Level Plugin CSS - Dashboard -->
+    <link href="../Librerias/css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+    <link href="../Librerias/css/plugins/timeline/timeline.css" rel="stylesheet">
+   
+
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="../Librerias/css/sb-admin.css" rel="stylesheet">
+    <script type="text/javascript" src="../Librerias/js/subir_documento.js"></script>
+
+</head>
+
+<body>
+
+   
+    <div id="wrapper">
+       
+        
+		<!--<h2>design by <a href="#" title="flash templates">flash-templates-today.com</a></h2>-->
+        
+	
+        
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="inicio_grupo_empresa.php">Inicio </a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-tasks fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+            
+            
+            
+            
+            
+            
+
+<div class="navbar-default navbar-static-side" role="navigation">
+                <div class="sidebar-collapse">
+                    <ul class="nav" id="side-menu">
+                        
+                        
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-files-o "></i> Documentos <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                
+                                <li>
+                                    <a href="#" >Subir Documentos <span class="fa arrow"></span></a>
+                                    <ul class="nav nav-second-level">
+                                    
+                                    <?php
+                                    
+                                    //include '../Modelo/conexion.php';
+                                    $conect = new conexion();
+                                    
+                                    $SeleccionrAsesor = $conect->consulta("SELECT NOMBRE_UA FROM inscripcion WHERE NOMBRE_UGE='$UsuarioActivo'");
+                                    
+                                    $Asesor = mysql_fetch_row($SeleccionrAsesor);
+                                    
+                                    $SeleccionarDocReq = $conect->consulta("SELECT NOMBRE_R FROM registro WHERE NOMBRE_U = '$Asesor[0]' AND TIPO_T='documento requerido' ");
+                                    
+                                    while ($rowDocs = mysql_fetch_row($SeleccionarDocReq))
+                                    {
+                                        echo '<li>
+                                                <a href="SubirDocumento.php?doc='.$rowDocs[0].'">'.$rowDocs[0].'</a>
+                                              </li>';    
+                                    }
+                                    ?>
+                                </ul>
+                                    
+                                  
+                                    
+                                </li>
+                                <li>
+                                    <a href="publicacion_grupo.php">Recepci&oacute;n Documentos </a>
+                                    
+                                </li>
+                               
+                            </ul>
+                            
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+                         <li>
+                            <a href="#"><i class="fa fa-tasks fa-fw"></i> Tareas<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <?php
+                                                 $idUsuarioAsesor='leticia';
+                                                 $idUsuarioG='freevalue';
+                                echo   ""
+                                     . "<form name='formularioNombre' action='verificar_nombre.php' enctype='multipart/form-data' method='POST'>"
+                                     . "<input type='hidden' name='nombreGrupo' value='$idUsuarioG'>"
+                                     . "<input type='hidden' name='nombreAsesor' value='$idUsuarioAsesor'>"
+                                     . "</form>"
+                                     . "<li>"
+                                     . "<a href='javascript:document.formularioNombre.submit();'>Verificar Nombre de Empresa</a>"
+                                     . "</li>";
+                                ?>
+                                <li>
+                                    <a href="seleccionar_asesor.php">Seleccionar Asesor</a>
+                                </li>
+                                
+                                 <li>
+                                     <a href="AnadirSocio.php">Añadir socios</a>
+                                </li>
+                                
+                                <li>
+                                    <a href="AnadirRL.php">Seleccionar Representante legal</a>
+                                </li>
+                                
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+                        <li>
+                            <a href="#"><i class="fa fa-warning fa-fw"></i> Notificaciones<span class="fa arrow"></span></a>
+                                                    
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="historia_actividades.php">Historia de actividades</a>
+                                </li>
+                                
+                            </ul>  
+                            </li>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-building-o fa-fw"></i> Actividades<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a id="registrarPlanificacion" href="#">
+                                        <i class="fa fa-pencil-square-o fa-fw"></i>Registrar Planificaci&oacute;n
+                                    </a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+                        <li>
+                            <a href="#"><i class="fa fa-question-circle fa-fw"></i> Ayuda <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        
+                        <li>
+                            <a href="lista-de-noticias-grupo.php"><i class="fa fa-comment"></i> Foro</a>
+                                
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                    <!-- /#side-menu -->
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            
+            
+            
+            
+            <!-- /.navbar-static-side -->
+        </nav>
+        
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="page-header">Subir Documento</h2>
+                    <div class="col-lg-6" >
+                        
+                        <?php
+                        
+                        $VerificarIns= $con->consulta("SELECT NOMBRE_U FROM inscripcion_proyecto WHERE NOMBRE_U = '$UsuarioActivo' ");
+                            $VerificarIns2 = mysql_fetch_row($VerificarIns);
+                            if (!is_array($VerificarIns2))
+                            {
+                                echo "<h4>Para subir su propuesta primero debe inscribirse a un proyecto</h4>";
+                            }
+                            else{  
+
+
+                                
+                                echo '
+                                    <form action="GuardarSubirDocumento.php" method="POST" enctype="multipart/form-data">
+                                        <fieldset>
+                                            <div class="form-group">
+                                                <input name="archivoA" id="archivoA" type="file" class = "btn btn-primary" required>
+                                            </div>
+                                        
+                                            <div class="form-group">
+                                                <input type="submit" value="Subir Documento" class= "btn btn-primary">
+                                            </div>
+                                        </fieldset>
+                                    </form>';
+                            }
+                        
+                        ?>
+                        
+                        
+                       
+                    </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Core Scripts - Include with every page -->
+    <script src="../Librerias/js/jquery-1.10.2.js"></script>
+    <script src="../Librerias/js/bootstrap.min.js"></script>
+    <script src="../Librerias/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+
+    <!-- Page-Level Plugin Scripts - Dashboard -->
+    <script src="../Librerias/js/plugins/morris/raphael-2.1.0.min.js"></script>
+    <script src="../Librerias/js/plugins/morris/morris.js"></script>
+
+    <!-- SB Admin Scripts - Include with every page -->
+    <script src="../Librerias/js/sb-admin.js"></script>
+
+    <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+    <script src="../Librerias/js/demo/dashboard-demo.js"></script>
+
+</body>
+
+</html>
